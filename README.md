d9c66446a32d96e61abd236ec34f850015b8787b
0xEnrico <0xenrico@gmail.com>
2022-07-09 11:58:12 +0200

    Implemented basic plot of Black Scholes implied volatilities

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # fengler-arbitrage-free
+
+## Requirements
+
+    conda install -c conda-forge cvxopt quadprog qpsolvers
+
+or
+
+    pip install cvxopt quadprog qpsolvers[starter_solvers]
+
+Python porting of Matlab implementation: https://github.com/marta-riva/duoGozzo/tree/master/IVS_Code
+Original paper: Arbitrage-Free Smoothing of the Implied Volatility Surface, Matthias R. Fengler, 2005 (https://core.ac.uk/reader/6978470)
---
+++ main.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pyarrow.compute as pc
+#from fengler import *
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+# This example reads SPX_2022_03_04_10_01_00.parquet file which contain SPX options data for 04 Mar 2022 10:01
+# and applies Fengler's arbitrage free interpolation [1], then plots the data
+#
+# [1] Arbitrage-Free Smoothing of the Implied Volatility Surface, Matthias R. Fengler, 2005 (https://core.ac.uk/reader/6978470)
+
+if __name__ == "__main__":
+
+    # Read option data from parquet file
+    option_data = pq.read_table("SPX_2022_03_04_10_01_00.parquet")
+    strikes = np.unique(np.array(option_data["K"]))
+    expiries = np.unique(np.array(option_data["T"]))
+    impl_vols_bid = np.zeros([len(expiries),len(strikes)])
+    impl_vols_ask = np.zeros([len(expiries),len(strikes)])
+    impl_vols = np.zeros([len(expiries),len(strikes)])
+    i = 0
+    for T in expiries:
+        option_data_single_expiry = option_data.filter(pa.compute.equal(option_data["T"], T))
+        impl_vols_bid[i] = np.array(option_data_single_expiry["IV_Bid"])
+        impl_vols_ask[i] = np.array(option_data_single_expiry["IV_Ask"])
+        impl_vols[i] = np.array(option_data_single_expiry["IV"])
+        i+=1
+    impl_vols_bid = np.where(impl_vols_bid<=0,np.nan,impl_vols_bid)
+    impl_vols_ask = np.where(impl_vols_ask<=0,np.nan,impl_vols_ask)
+    impl_vols = np.where(impl_vols<=0,np.nan,impl_vols)
+
+    # Plot option data as total implied variance
+    mpl.rcParams['lines.linewidth'] = 1
+    i = 0
+    for T in expiries:
+        plt.scatter(strikes, impl_vols_bid[i]*impl_vols_bid[i]*T, marker=mpl.markers.CARETUP, c='b')
+        plt.scatter(strikes, impl_vols_ask[i]*impl_vols_ask[i]*T, marker=mpl.markers.CARETDOWN, c='b')
+        plt.scatter(strikes, impl_vols[i]*impl_vols[i]*T, marker='.', c='b')
+        i+=1
+    plt.show()
